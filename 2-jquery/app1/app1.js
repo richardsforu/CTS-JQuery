3048e6613427a7b75271105ab96a1cf4ce34cb62
richardsforu <praveen.somireddy@gmail.com>
2021-04-30 16:52:55 +0530

    jquery selectors

--- 2-jquery/app1/app1.js
+++ 2-jquery/app1/app1.js
@@ -12,19 +12,49 @@ $(document).ready(function(){
 
 */
 
-$(document).ready(() => {
-
-    var price = $("<p class='text-success text-center'>From $399.99</p>");
-
-    $('.vacation').on('click', 'button', (e) => {
-
+$(document).ready(function () {
+    $(".price").on("click", function () {
         var vacation = $(this).closest('.vacation');
-       //$(this).before('.vacation');
-        // price.appendTo($('.vacation'));    
-        vacation.append(price);    
+
+        var amount = vacation.data('price');
+
+        var price = $(`<p>&#8377;${amount}</p>`);
+
+        console.log(amount);
+        // $(this).closest('.vacation').append(price);
+        vacation.append(price);
+        $(this).remove();
+
+    });
+
+
+    //-------------
+
+    $('#filters').on('click','.btn-success',function () {
+       // console.log('On sale');'
+       $('.bg-warning').removeClass("bg-warning");
+       $('.vacation').filter('.onsale').addClass('bg-info');
+    })
+
+
+    $('#filters').on('click','.btn-danger',function () {
+        //console.log('Expiring soon');    
+        $('.bg-info').removeClass("bg-info");
+   
+        $('.vacation').filter('.expiring').addClass('bg-warning');
+
 
     })
 
 
 
 })
+
+
+
+
+
+
+
+
+
